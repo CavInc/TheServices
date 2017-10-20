967e45305e357eea363d6b064d08f35cd49bfa59
CavInc <cavinc20@gmail.com>
2017-10-20 10:07:53 +0700

    Init Commit

---
+++ app/src/main/java/cav/theservices/ui/activitys/MainActivity.java
@@ -0,0 +1,15 @@
+package cav.theservices.ui.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cav.theservices.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
