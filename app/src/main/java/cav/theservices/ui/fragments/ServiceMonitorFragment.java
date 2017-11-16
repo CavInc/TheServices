28df1c6767e8c613347c0ced9c724fc1c2f37eaa
CavInc <cavinc20@gmail.com>
2017-11-16 14:48:59 +0700

    Немножко добавлены методы в класс

--- app/src/main/java/cav/theservices/ui/fragments/ServiceMonitorFragment.java
+++ app/src/main/java/cav/theservices/ui/fragments/ServiceMonitorFragment.java
@@ -1,6 +1,7 @@
 package cav.theservices.ui.fragments;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,13 +9,27 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import cav.theservices.R;
+import cav.theservices.data.managers.DataManager;
 
 public class ServiceMonitorFragment extends Fragment{
+
+    private DataManager mDataManager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDataManager = DataManager.getInstance();
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.service_monitor_fragment,container,false);
+
+        if (mDataManager.isOnline()) {
+            // запрашиваем даные с командного сервера.
+        }
+
         return view;
     }
 }
