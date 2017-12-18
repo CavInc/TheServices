5dd4868ee8da4fbe32e2efd57a6f69eb155809c0
CavInc <cavinc20@gmail.com>
2017-12-18 13:44:28 +0700

    Поправлено отображение удаленного заказа

--- app/src/main/java/cav/theservices/data/database/DBConnect.java
+++ app/src/main/java/cav/theservices/data/database/DBConnect.java
@@ -100,9 +100,9 @@ public class DBConnect {
     }
 
     public Cursor getLimitService(int lang,int start,int limit){
-        --
+
         String sql="select sh.id,sh.price,sh.icon_file,sh.price,sp.title,sp.body from "+DBHelper.SERVICE_HEAD_TABLE+" sh"+
-                " left join "+DBHelper.SERVICE_SPEC_TABLE+" sp on sh.id=sp.id and sp.lang_id ="+lang+" limit "+limit+" offset "+start;
+                " left join "+DBHelper.SERVICE_SPEC_TABLE+" sp on sh.id=sp.id and sp.lang_id ="+lang+" where sh.status<>99 limit "+limit+" offset "+start;
 
         Cursor cursor = database.rawQuery(sql,null);
         return cursor;
